2b0f048540a379bd5d92cb8f0cbadeeea1633cfd
Adeline <50517260+Ad3lineL@users.noreply.github.com>
2026-05-24 15:25:36 +0200

    loottabledatagen setup

--- src/main/java/com/addyberry/blocksabound/datagen/BlocksAboundDatagen.java
+++ src/main/java/com/addyberry/blocksabound/datagen/BlocksAboundDatagen.java
@@ -1,4 +1,31 @@
 package com.addyberry.blocksabound.datagen;
 
+import com.addyberry.blocksabound.BlocksAbound;
+import com.addyberry.blocksabound.datagen.providers.BlocksAboundLootTableProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@EventBusSubscriber(modid = BlocksAbound.MODID)
 public class BlocksAboundDatagen {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput output = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new LootTableProvider(output, Collections.emptySet(),
+                List.of(new LootTableProvider.SubProviderEntry(BlocksAboundLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
+    }
 }
--- src/main/java/com/addyberry/blocksabound/datagen/providers/BlocksAboundLootTableProvider.java
+++ src/main/java/com/addyberry/blocksabound/datagen/providers/BlocksAboundLootTableProvider.java
@@ -10,18 +10,20 @@ import net.minecraft.world.level.block.Block;
 import java.util.Set;
 
 public class BlocksAboundLootTableProvider extends BlockLootSubProvider {
-    protected BlocksAboundLootTableProvider(HolderLookup.Provider registries) {
+    public BlocksAboundLootTableProvider(HolderLookup.Provider registries) {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
-    }
-
-    @Override
-    protected void generate() {
-
     }
 
     @Override
     protected Iterable<Block> getKnownBlocks() {
         return BlocksAboundBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    }
+
+    @Override
+    protected void generate() {
+        for (Block block : this.getKnownBlocks()) {
+            dropSelf(block);
+        }
     }
 
 }
