a27fc51b35093b10e52f83f5ff74114f8c22fa63
MonkyMonka <daniyaldanish11dd@gmail.com>
2026-05-24 11:23:49 -0400

    blockstate provider entry

--- src/main/java/com/addyberry/blocksabound/datagen/providers/BABlockstateProvider.java
+++ src/main/java/com/addyberry/blocksabound/datagen/providers/BABlockstateProvider.java
@@ -2,6 +2,7 @@ package com.addyberry.blocksabound.datagen.providers;
 
 import com.addyberry.blocksabound.BlocksAbound;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.client.model.generators.ModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -16,7 +17,7 @@ public class BABlockstateProvider extends BlockStateProvider {
     @Override
     protected void registerStatesAndModels() {
 
-        this.simpleBlockWithItem(TAWNY_PLATE.get(), cubeAll(TAWNY_PLATE.get())
-        );
+        ResourceLocation tawnyPlateLoc = this.modLoc(ModelProvider.BLOCK_FOLDER + "/tawny_plate/tawny_plate");
+        this.simpleBlockWithItem(TAWNY_PLATE.get(), this.models().cubeAll("tawny_plate", tawnyPlateLoc));
     }
 }
