5e46ba2b14ff407fe0a6534bb6a0e4b781c5e81a
tazer <tazercopter@gmail.com>
2026-06-16 14:57:48 +0300

    fix large plate stickiness

--- src/main/java/com/addyberry/blocksabound/mixin/LargePlateStickinessMixin.java
+++ src/main/java/com/addyberry/blocksabound/mixin/LargePlateStickinessMixin.java
@@ -8,11 +8,28 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.piston.PistonStructureResolver;
 import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(PistonStructureResolver.class)
 public class LargePlateStickinessMixin {
+
+    @Shadow
+    @Final
+    private Direction pushDirection;
+
+    @WrapOperation(method = "addBlockLine", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;canStickTo(Lnet/minecraft/world/level/block/state/BlockState;)Z", ordinal = 1))
+    private boolean blocksabound$onlyConnectFacingPlatesS(BlockState instance, BlockState state, Operation<Boolean> original) {
+        boolean canStickTo = original.call(instance, state);
+        if (canStickTo && instance.getBlock() instanceof LargePlateBlock) {
+            Direction facing = instance.getValue(LargePlateBlock.FACING);
+            if (pushDirection.getOpposite() != facing) return false;
+        }
+
+        return canStickTo;
+    }
 
     @WrapOperation(method = "addBranchingBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;canStickTo(Lnet/minecraft/world/level/block/state/BlockState;)Z", ordinal = 1))
     private boolean blocksabound$onlyConnectFacingPlates(BlockState instance, BlockState state, Operation<Boolean> original, @Local(ordinal = 1) BlockPos pos, @Local Direction direction) {
