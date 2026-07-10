78b4ef07c60afc12eea1d4102b69fdb3481233a5
Adeline <50517260+Ad3lineL@users.noreply.github.com>
2026-07-10 02:36:10 +0200

    Correct vent hitbox

--- src/main/java/com/addyberry/blocksabound/common/blocks/VentBlock.java
+++ src/main/java/com/addyberry/blocksabound/common/blocks/VentBlock.java
@@ -26,6 +26,12 @@ public class VentBlock extends AbstractPanelBlock{
     protected static final VoxelShape NORTH_AABB = Block.box(0.0D, 0.0D, 14.0D, 16.0D, 16.0D, 16.0D);
     protected static final VoxelShape UP_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
     protected static final VoxelShape DOWN_AABB = Block.box(0.0D, 14.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape EAST_CONNECTED_AABB = Block.box(-2.0D, 0.0D, 0.0D, 2.0D, 16.0D, 16.0D);
+    protected static final VoxelShape WEST_CONNECTED_AABB = Block.box(14.0D, 0.0D, 0.0D, 18.0D, 16.0D, 16.0D);
+    protected static final VoxelShape SOUTH_CONNECTED_AABB = Block.box(0.0D, 0.0D, -2.0D, 16.0D, 16.0D, 2.0D);
+    protected static final VoxelShape NORTH_CONNECTED_AABB = Block.box(0.0D, 0.0D, 14.0D, 16.0D, 16.0D, 18.0D);
+    protected static final VoxelShape UP_CONNECTED_AABB = Block.box(0.0D, -2.0D, 0.0D, 16.0D, 2.0D, 16.0D);
+    protected static final VoxelShape DOWN_CONNECTED_AABB = Block.box(0.0D, 14.0D, 0.0D, 16.0D, 18.0D, 16.0D);
 
     public VentBlock(Properties properties) {
         super(properties);
@@ -43,16 +49,17 @@ public class VentBlock extends AbstractPanelBlock{
     }
 
     public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        boolean flag = state.getValue(CONNECTED);
         return switch (state.getValue(FACE)) {
-            case FLOOR -> UP_AABB;
+            case FLOOR -> flag ? UP_CONNECTED_AABB : UP_AABB;
             case WALL -> switch (state.getValue(FACING)) {
-                case NORTH -> NORTH_AABB;
-                case SOUTH -> SOUTH_AABB;
-                case WEST -> WEST_AABB;
-                case EAST -> EAST_AABB;
+                case NORTH -> flag ? NORTH_CONNECTED_AABB : NORTH_AABB;
+                case SOUTH -> flag ? SOUTH_CONNECTED_AABB : SOUTH_AABB;
+                case WEST -> flag ? WEST_CONNECTED_AABB : WEST_AABB;
+                case EAST -> flag ? EAST_CONNECTED_AABB : EAST_AABB;
                 default -> throw new MatchException(null, null);
             };
-            default -> DOWN_AABB;
+            default -> flag ? DOWN_CONNECTED_AABB : DOWN_AABB;
         };
     }
 
